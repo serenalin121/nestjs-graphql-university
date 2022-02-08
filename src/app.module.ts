0a36297037ad0682bb1ad1b1dfbe4f4563188cd2
serenalin121 <serenalin121@gmail.com>
2022-02-08 14:55:34 -0800

    enable graphql playground

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UniversitiesModule } from './universities/universities.module';
 
 @Module({
-  imports: [UniversitiesModule],
+  imports: [
+    UniversitiesModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      debug: true,
+      playground: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
